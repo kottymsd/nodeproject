7fe3f0de6e5ca206f3fd85708bbd06ec146814f1
root <root@ip-172-31-2-93.ap-south-1.compute.internal>
2026-01-15 15:08:51 +0000

    newchangedevelop

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
-  res.end('Teamchange2 revision of Deployment is Successful on Linux!');
+  res.end('Teamchange3 revision of Deployment is Successful on Linux!');
 });
 
 server.listen(3000);
